1f9087770e5174e0457436de836443a0f39b16a4
NatalJR <juniortl2005@gmail.com>
2018-04-13 17:19:52 -0300

    Local Encaminhado corrigido

--- sigtrans-application/src/main/java/br/sigtrans/sigtrans/repository/LocalEncaminhadoRepository.java
+++ sigtrans-application/src/main/java/br/sigtrans/sigtrans/repository/LocalEncaminhadoRepository.java
@@ -1,8 +1,9 @@
 package br.sigtrans.sigtrans.repository;
 
+import br.sigtrans.sigtrans.domain.LocalEncaminhado;
 import io.gumga.domain.repository.GumgaCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LocalEncaminhadoRepository extends GumgaCrudRepository {
+public interface LocalEncaminhadoRepository extends GumgaCrudRepository <LocalEncaminhado, String> {
 }
