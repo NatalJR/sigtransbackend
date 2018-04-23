82c508a1dc9987f4e58f3c78b2571f4ec821105f
NatalJR <juniortl2005@gmail.com>
2018-04-23 20:08:13 -0300

    Pesquisa por nome (ESTADO)

--- sigtrans-api/src/main/java/br/sigtrans/sigtrans/api/resource/EstadoResource.java
+++ sigtrans-api/src/main/java/br/sigtrans/sigtrans/api/resource/EstadoResource.java
@@ -1,12 +1,16 @@
 package br.sigtrans.sigtrans.api.resource;
 
 import br.sigtrans.sigtrans.domain.Estado;
+import br.sigtrans.sigtrans.service.EstadoService;
 import io.gumga.application.GumgaService;
 import io.gumga.presentation.GumgaAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/estado")
@@ -14,5 +18,10 @@ public class EstadoResource extends GumgaAPI<Estado, String>{
     @Autowired
     public EstadoResource(@Qualifier("estadoService")GumgaService<Estado, String> service){
         super(service);
+    }
+
+    @RequestMapping("/novo")
+    public List<Estado> BuscarEstadoGQuery(@RequestParam String nome) {
+        return ((EstadoService) service).buscarEstadoPorNomeGQuery(nome);
     }
 }
--- sigtrans-application/src/main/java/br/sigtrans/sigtrans/repository/EstadoRepository.java
+++ sigtrans-application/src/main/java/br/sigtrans/sigtrans/repository/EstadoRepository.java
@@ -1,9 +1,18 @@
 package br.sigtrans.sigtrans.repository;
 
 import br.sigtrans.sigtrans.domain.Estado;
+import io.gumga.domain.domains.GumgaOi;
 import io.gumga.domain.repository.GumgaCrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EstadoRepository extends GumgaCrudRepository<Estado, String> {
+
+    @Query("from Estado as c where c.nome like:nome")
+    List<Estado> buscaEstadoPorNome(@Param("nome") String nome);
+
 }
